2821deb8a9a2bf7a6e619fe232956c14125696cc
balaji <mohanty1balaji@gmail.com>
2024-12-05 16:52:30 +0530

    ihave changed weather app

--- first/src/api/Weather.jsx
+++ first/src/api/Weather.jsx
@@ -11,7 +11,7 @@ function Weather() {
  
   const handleWeather = () => {
     if (!city) {
-      setError("Please enter a city name.");
+      setError("Please enter a city .");
       return;
     }
  
